332ad17ffe846a5ca719e9a78ad7f753e2b5d8d9
Yuvraj-Nirale-GD <yuvrajnirale@zohomail.in>
2026-01-28 20:14:12 +0530

    replaced old logic with logic suitable for Web-Based application

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Habit Tracker (CLI)
 
-A Python tool to track daily habits and calculate consistency. 
+## Overview
+Habit Tracker is a simple application built using Python and SQLite that helps users track daily habits and maintain consistency over time.
+
+This project was created to understand file handling, database integration, and basic application flow in Python.
 
 I built this because I needed a way to track Our daily habits and studies without using complex apps that sell my data. The goal is to build the core logic here first, then add a UI and AI analysis later.
 
@@ -10,8 +13,31 @@ I built this because I needed a way to track Our daily habits and studies withou
 - **Saves Data:** Uses SQLite so data persists after the script closes.
 - **Analytics:** The `report` command calculates consistency percentages and average intensity.
 
-## How to run it
-1. Clone this repo.
-2. Run the script:
+## Features
+- Add new habits
+- Mark habits as completed
+- Store habit data persistently using SQLite
+- View habit completion history
+
+## Tech Stack
+- Python
+- SQLite
+- Command Line Interface (CLI)
+
+## Project Structure
+- `main.py` – Application entry point
+- `database.py` – Handles SQLite database operations
+- `habits.db` – Local database file
+
+## How to Run
+1. Clone the repository
    ```bash
-   python main.py
+   git clone https://github.com/Yuvraj-Nirale-GD/Habit-Tracker.git
+   ```
+2. Nevigate to the project Directory
+   ```cd Habit-Tracker
+   ```
+3. Run the Appication
+   ```python main.py
+   ```
+
--- main.py
+++ main.py
@@ -1,102 +1,149 @@
-"""
-Project: Iron-Habit (Backend Core)
-Author: Yuvraj Nirale
-Status: Phase 1 (CLI & Logic)
+# """
+# Project: Iron-Habit (Backend Core)
+# Author: Yuvraj Nirale
+# Status: Phase 1 (CLI & Logic)
 
-Description: 
-    The core logic engine for the Habit tracker. This script handles:
-    1. Database interactions (SQLite).
-    2. Input sanitization and validation.
-    3. Mathematical calculation of consistency scores.
+# Description: 
+#     The core logic engine for the Habit tracker. This script handles:
+#     1. Database interactions (SQLite).
+#     2. Input sanitization and validation.
+#     3. Mathematical calculation of consistency scores.
     
-    NOTE: This is the precursor to the planned GUI version. 
-"""
+#     NOTE: This is the precursor to the planned GUI version. 
+# """
 
-from datetime import date
+from datetime import date, datetime
 import sqlite3
 
 Db_habit = sqlite3.connect('habits.db')
 cursor = Db_habit.cursor()
+# cursor.execute(' DROP TABLE Daily_Log;'
+# )
+# Db_habit.commit
 cursor.execute('''
                CREATE TABLE IF NOT EXISTS habits(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Task TEXT,
+                Description TEXT,
+                Creation_Date TEXT
+               )'''
+            )
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS Daily_Log(
                id INTEGER PRIMARY KEY AUTOINCREMENT,
+               habit_id INTEGER,
                date TEXT,
-               task TEXT,
-               status BOOL,
-               efforts INTEGER)'''
+               Status BOOLEAN,
+               efforts INTEGER,
+               FOREIGN KEY (habit_id) REFERENCES habits(id)
+               )'''
             )
 Db_habit.commit()
-today_date = date.today().strftime("%Y-%m-%d") # strftime to format date in YYYY-MM-DD
 
-while True:
-    habit_name = input("Enter your Daily Habit (type 'exit' to stop): ").strip().lower()
+def  Create_Habit():
+    start_date = date.today().strftime("%Y-%m-%d") # strftime to format date in YYYY-MM-DD
     
-    if habit_name == '':
-        print("Task cannot be empty")
-        continue
+    while True:
+        habit_name = input("Create a Task: ").strip().lower()
 
-    if habit_name == 'exit':
-        break
-
-    elif habit_name == 'report':
-        cursor.execute('''
-                       SELECT task,
-                       COUNT(*) AS total,
-                       SUM(status) AS completed,
-                       AVG(efforts) AS avg_effort
-                       FROM habits
-                       GROUP BY task
-                       ''')
-        
-        report = cursor.fetchall()
-
-        if not report:
-            print('No habits logged yet.')
-            continue
-        else:
-            print("\n--- Habit Report ---")
-            for row in report:
-                task_name = row[0]
-                total = row[1]
-                completed = row[2]  
-                ave_effort = row[3]
-
-                consistancy = (completed/total)*100 
-
-                print(f"Habit: {task_name},\nConsistency: {consistancy:.1f}%,\nAverage Effort: {ave_effort:.1f}")
-        
-        if input("do you want to continue? (type yes to continue)").strip().lower() == 'yes':
+        if habit_name == '':
+            print("Task cannot be empty")
             continue
         else:
             break
-        
-    cursor.execute('''
-                   SELECT * FROM habits WHERE date = ? AND task = ?''',
-                   (today_date, habit_name)
-                   )
+    Description = input("Enter Description for the Habit: ").strip()
+    # Check if habit already exists
+    cursor.execute('SELECT ID FROM habits WHERE task = ?',(habit_name,))
+
     existing_entry = cursor.fetchone() #if fetchone returns None, means no existing entry
+
     if existing_entry:
-        print(f"You have already logged the habit '{habit_name}' for today.")
-        continue            
+        print(f"You have already logged the habit '{habit_name}' ")
+    else:
+        cursor.execute('''
+                       INSERT INTO habits(Task, Description, Creation_Date)
+                       VALUES (?, ?, ?)''',(habit_name, Description, start_date))
 
-    status = input(f"Did you complete '{habit_name}' today? (yes/no): ").strip().lower()
-    efforts = 0
-    if status == "yes": 
-        while True:
-            try:
-                efforts = int(input(f"How much Effort did you put into completing '{habit_name}'? (1-10): "))
-                if (1 <= efforts <= 10):
-                    break
-                else:
-                    print("bruhh, Effort must be between 1 and 10.")
-                    continue
-            except ValueError:
-                print("Invalid input. Please enter a number between 1 and 10 for Effort.")
+        Db_habit.commit()
+        print(f"Habit '{habit_name}' created successfully!")
+    
+     
+def Daily_Log_Insert():
+    today_date= date.today().strftime("%Y-%m-%d")
 
-    cursor.execute('''
-                   INSERT INTO habits (date, task, status, efforts)
-                   VALUES (?, ?, ?, ?)''',
-                   (today_date, habit_name, status == 'yes', efforts)# status == 'yes' converts to boolean, if it is no then False
-                    )
-    Db_habit.commit()
-    print(f"Habit {habit_name} logged successfully!")
+    cursor.execute('SELECT id, Task, Description FROM habits')
+    habits = cursor.fetchall() # habit is tuple
+
+    if not habits:
+        print(f"There is no Task logged")
+        return
+        
+    for habit in habits:
+        habit_id, habit_name, dec = habit
+
+        print(f"Task : {habit_name.upper()}\n Description = ({dec})")
+        status = input(f"Did you complete '{habit_name}' today? (y/n): ").strip().lower()
+        
+        is_done = 1 if status == 'y' else 0
+        effort = 0
+
+        if is_done:
+            while True:
+                try:
+                    effort = int(input("enter Eforts you've Put(0-10) :"))
+                    if 0 <= effort <= 10:
+                        break
+                except ValueError : pass
+        
+        cursor.execute('''
+                INSERT INTO Daily_Log(habit_id, date, status, efforts)
+                VALUES (?,?,?,?)''', (habit_id, today_date, is_done, effort))
+        Db_habit.commit()
+        print(f"Task Logged Successfully")
+
+def Analysis():
+    cursor.execute('SELECT id, Task, Creation_Date FROM habits')
+    habit = cursor.fetchall()
+
+    for hbt in habit:
+        h_id = hbt[0]
+        name=hbt[1]
+        crt_dt = hbt[2]
+
+        start_date = datetime.strptime(crt_dt, "%Y-%m-%d").date() #coverting string from database back to date 
+        days_passed = (date.today() - start_date).days + 1 # adding one for today
+
+        cursor.execute('SELECT COUNT(*), AVG(efforts) FROM Daily_Log WHERE habit_id =? AND status =1', (h_id,))
+        row = cursor.fetchone()
+        totalCount= row[0]
+        Average_effort = row[1] if row[1] else 0
+
+    
+
+        Consistancy = (totalCount/days_passed)*100
+
+        print(f"Task: {name.upper()}")
+        print(f"   Since: {start_date} ({days_passed} days ago)")
+        print(f"   Done: {totalCount} times")
+        print(f"   Consistency: {Consistancy:.1f}%")
+        print(f"   Average Effort: {Average_effort:.1f}/10")
+
+        if Consistancy < 50:
+            print("you are not putting your all efforts")
+        elif 50< Consistancy <70:
+            print(" you can do more, why are you not pushing your limits")
+        else:
+            print(" congrats! You are above your other competitors")
+
+
+while True:
+    print("\n1: Create Task | 2: Daily Check | 3: Analysis | 4: Exit")
+    try:
+        choice = int(input("Choose Operation: "))
+        if choice == 1: Create_Habit()
+        elif choice == 2: Daily_Log_Insert()
+        elif choice == 3: Analysis()
+        elif choice == 4: break
+        else: print("Invalid Input")
+    except ValueError:
+        print("Please enter a number.")
